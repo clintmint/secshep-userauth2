7ecb50f44cde487f270630d7239707e8c5b4532c
Clinton Minton <clintonminton@gmail.com>
2022-10-11 22:23:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,9 @@ import java.util.Date;
 
 class Main {
   public static void main(String[] args) {
-    // Create a new date object instance
-    Date newDate = new Date();
-    
-    // Convert date to string
-    String dateToString = String.valueOf(newDate);
-    
-    // base64 encode string
-    String resetPasswordToken = Base64.getEncoder().encodeToString(dateToString.getBytes());
-
-    // Print the token
-    System.out.println("Use this token: " + resetPasswordToken);
+    Date dateObj = new Date();
+    String dateStr = String.valueOf(dateObj);
+    String passwordResetToken = Base64.getEncoder().encodeToString(dateStr.getBytes());
+    System.out.println("Use this token: " + passwordResetToken);
   }
 }
